309a1af40add6c8686174c8f7fcd3c324d74d9f3
NidaSyeda <syedanidasaher@gmail.com>
2026-04-01 01:02:25 +0530

    initital commit

--- accountss/settings.py
+++ accountss/settings.py
@@ -93,12 +93,12 @@ STATIC_ROOT = BASE_DIR / "staticfiles_build"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
+ }
 
 
 # Password validation
